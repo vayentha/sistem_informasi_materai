83a07316b72a0efae23767bafecc8f3473a49e06
reyhan audian dwi putra <reyhanadp@yahoo.co.id>
2018-04-05 19:13:32 +0700

    first commit

---
+++ src/sampulfbk/Sampulfbk.java
@@ -0,0 +1,17 @@
+
+package sampulfbk;
+
+/**
+ *
+ * @author jauhar
+ */
+public class Sampulfbk {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
